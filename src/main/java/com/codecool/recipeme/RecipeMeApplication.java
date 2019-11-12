52cb5534b1599f58f5203e2b90b114c1a223f2c1
Verhóczki Márton <verhoczki.marci@gmail.com>
2019-11-12 10:02:30 +0100

    Initial commit

---
+++ src/main/java/com/codecool/recipeme/RecipeMeApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.recipeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/recipeme/RecipeMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.recipeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
