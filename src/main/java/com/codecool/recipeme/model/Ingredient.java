e73bd4cb455f73aa0f9d98d8fb54b9d4940e8a4c
Verhóczki Márton <verhoczki.marci@gmail.com>
2019-11-12 11:25:01 +0100

    Add Recepie class and its composition class Ingredient.

---
+++ src/main/java/com/codecool/recipeme/model/Ingredient.java
@@ -0,0 +1,22 @@
+package com.codecool.recipeme.model;
+
+public class Ingredient {
+    String text;
+    Double weight;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+}
