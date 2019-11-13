84f8ccc4c5dc1313646604dffed177e61a9a626a
Verhóczki Márton <verhoczki.marci@gmail.com>
2019-11-13 13:27:00 +0100

    Add RecipeSearch Service class

---
+++ src/main/java/com/codecool/recipeme/service/RecipeSearch.java
@@ -0,0 +1,24 @@
+package com.codecool.recipeme.service;
+
+import com.codecool.recipeme.model.Search;
+import com.codecool.recipeme.model.generated.HitsItem;
+import com.codecool.recipeme.model.generated.Recipe;
+import com.codecool.recipeme.model.generated.Response;
+import com.codecool.recipeme.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class RecipeSearch {
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    public List<Recipe> getRecipesBySearch(Search search) throws IllegalAccessException {
+        Response response = recipeRepository.getResponse(search);
+        return response.getHits().stream().map(HitsItem::getRecipe).collect(Collectors.toList());
+    }
+}
