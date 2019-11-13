4201671a7e9b298d5604a1986ca77bf520b240a9
Verhóczki Márton <verhoczki.marci@gmail.com>
2019-11-13 15:22:36 +0100

    Refactor RecipeRepository to use String building for get request and map response to Response class

--- src/main/java/com/codecool/recipeme/repository/RecipeRepository.java
+++ src/main/java/com/codecool/recipeme/repository/RecipeRepository.java
@@ -2,26 +2,50 @@ package com.codecool.recipeme.repository;
 
 import com.codecool.recipeme.model.Search;
 import com.codecool.recipeme.model.generated.Response;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import com.codecool.recipeme.service.RecipeSearch;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import static com.codecool.recipeme.util.RequestBuilder.getParameterMap;
 
 @Component
-public class RecipeRepository {
-    private static final String apiUrl = "";
+public class RecipeRepository implements ApiRepository {
+    @Value("${api.url.recipe}")
+    String apiUrl;
 
-    public Response getResponse(Search search) throws IllegalAccessException {
+    @Value("${api.key.recipe}")
+    String APIKEY;
+
+    @Value("${api.id.recipe}")
+    String APIID;
+
+    @Autowired
+    RecipeSearch recipeSearch;
+
+    public Response getResponse(Search search) throws IllegalAccessException, NoSuchFieldException {
         RestTemplate restTemplate = new RestTemplate();
-        /*HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);*/
-        MultiValueMap parameterMap = getParameterMap(search);
-        HttpEntity request = new HttpEntity(parameterMap);
-        ResponseEntity<Response> response = restTemplate.exchange(apiUrl, HttpMethod.GET, request, Response.class);
-        return response.getBody();
+        String queryString = getParameterMap(search);
+        String credentialString = getApiCredentials();
+        String url = new StringBuilder().append(apiUrl).append(queryString).append(credentialString).toString();
+        Response response = restTemplate.getForObject(url, Response.class);
+        return response;
+    }
+
+    @Override
+    public String getApiKey() {
+        return APIKEY;
+    }
+
+    @Override
+    public String getApiId() {
+        return APIID;
+    }
+
+    @Override
+    public String getApiCredentials() {
+        String credentialString = new StringBuilder().append("&app_id=").append(APIID).append("&app_key=").append(APIKEY).toString();
+        return credentialString;
     }
 }
