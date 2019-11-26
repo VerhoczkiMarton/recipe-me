facafec8e158f6d533cc9e68e3357f801838b908
Daniel Kiraly <danielkiraly@ymail.com>
2019-11-26 09:57:34 +0100

    Add Crossorigins

--- src/main/java/com/codecool/recipeme/controller/SearchController.java
+++ src/main/java/com/codecool/recipeme/controller/SearchController.java
@@ -4,12 +4,14 @@ import com.codecool.recipeme.model.Search;
 import com.codecool.recipeme.model.generated.Recipe;
 import com.codecool.recipeme.service.RecipeSearch;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class SearchController {
 
