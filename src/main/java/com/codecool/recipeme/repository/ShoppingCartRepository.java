c6c4ccb3a5d19ec2f66471fc9d9c1fb4ed878845
Verhóczki Márton <verhoczki.marci@gmail.com>
2019-11-26 15:21:05 +0100

    Add repository layer for Shopping cart

--- src/main/java/com/codecool/recipeme/repository/ShoppingCartRepository.java
+++ src/main/java/com/codecool/recipeme/repository/ShoppingCartRepository.java
@@ -1,12 +1,10 @@
 package com.codecool.recipeme.repository;
 
-import com.codecool.recipeme.model.database.RecipeDB;
+import com.codecool.recipeme.model.generated.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 
 @Component
-public interface ShoppingCartRepository extends JpaRepository<RecipeDB, Long> {
-    public List<RecipeDB> findAll();
+public interface ShoppingCartRepository extends JpaRepository<Recipe, Long> {
 }
