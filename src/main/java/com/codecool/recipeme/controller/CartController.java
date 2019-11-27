1615a361aadb71ae88df3c6c8f45eac48fee0057
Verhóczki Márton <verhoczki.marci@gmail.com>
2019-11-27 12:37:27 +0100

    Hotfix Disable CORS for /api/cart for react

--- src/main/java/com/codecool/recipeme/controller/CartController.java
+++ src/main/java/com/codecool/recipeme/controller/CartController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController()
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
 @RequestMapping("/api/cart")
 public class CartController {
 
